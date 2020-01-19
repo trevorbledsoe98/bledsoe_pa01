45298f9f02a8dc6a09ad0bc162c1829248f8648c
Trevor Bledsoe <trevorbledsoe98@gmail.com>
2020-01-18 20:05:15 -0500

    first assignment

---
+++ src/bledsoe_pa01/GoodbyeWorld.java
@@ -0,0 +1,12 @@
+package bledsoe_pa01;
+
+public class GoodbyeWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.printf("Goodbye, World!");
+		
+	}
+
+}
---
+++ src/bledsoe_pa01/HelloWorld.java
@@ -0,0 +1,12 @@
+package bledsoe_pa01;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello, World!");
+		
+	}
+
+}
